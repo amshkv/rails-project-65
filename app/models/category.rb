8d6a07b0eec2836b81460963e05ea0bb3ae9163e
Andrey Moshkov <fakesupp@gmail.com>
2023-06-21 23:24:39 +0300

    :sparkles: Add categories

---
+++ app/models/category.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: categories
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Category < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 3, maximum: 30 }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_13_181902) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_21_202243) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
