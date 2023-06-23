942608c0f542f46dedc0751baf7868395c477771
Andrey Moshkov <fakesupp@gmail.com>
2023-06-23 22:56:23 +0300

    :art: Sort bulletins on created at

--- app/controllers/web/bulletins_controller.rb
+++ app/controllers/web/bulletins_controller.rb
@@ -2,6 +2,6 @@
 
 class Web::BulletinsController < Web::ApplicationController
   def index
-    @bulletins = Bulletin.all.includes(image_attachment: :blob)
+    @bulletins = Bulletin.all.includes(image_attachment: :blob).order(created_at: :desc)
   end
 end
