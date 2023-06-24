5c03a0b7a74556c621b0a56f930af28796bb8abe
Andrey Moshkov <fakesupp@gmail.com>
2023-06-25 01:01:29 +0300

    :sparkles: Show bulletins on welcome admin page

--- app/controllers/web/admin/welcome_controller.rb
+++ app/controllers/web/admin/welcome_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class Web::Admin::WelcomeController < Web::Admin::ApplicationController
-  def index; end
+  def index
+    @bulletins = Bulletin.all.order(created_at: :desc)
+  end
 end
