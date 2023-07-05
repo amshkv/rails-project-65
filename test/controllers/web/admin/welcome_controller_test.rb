cc0c8ef626e21d12f5e2c53425d81deee4268953
Andrey Moshkov <fakesupp@gmail.com>
2023-07-05 17:49:53 +0300

    :white_check_mark: Add not admin test for admins page

--- test/controllers/web/admin/welcome_controller_test.rb
+++ test/controllers/web/admin/welcome_controller_test.rb
@@ -3,13 +3,21 @@
 require 'test_helper'
 
 class Web::Admin::WelcomeControllerTest < ActionDispatch::IntegrationTest
-  # TODO: проверяем на админство один раз или несколько?
-  test 'guest should raise error from admin' do
+  # TODO: проверяем на админство один раз или во всех контроллерах?
+  test 'should guest cant get admin page' do
     get admin_root_url
     assert_response :redirect
   end
 
-  test 'signed user should get admin' do
+  test 'should not admin cant get admin page' do
+    user = users(:not_admin)
+    sign_in(user)
+
+    get admin_root_url
+    assert_response :redirect
+  end
+
+  test 'should admin signed user get admin page' do
     user = users(:full)
     sign_in(user)
 
