ae4191551e48e6d74c9aed70961aee88e29f4f6a
Andrey Moshkov <fakesupp@gmail.com>
2023-07-06 16:52:09 +0300

    :recycle: Use not hexlet way for env omni

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV.fetch('GITHUB_CLIENT_ID'), ENV.fetch('GITHUB_CLIENT_SECRET'), scope: 'user'
+  # TODO: NOT HEXLET WAY FETCH WITH NIL
+  provider :github, ENV.fetch('GITHUB_CLIENT_ID', nil), ENV.fetch('GITHUB_CLIENT_SECRET', nil), scope: 'user'
 end
