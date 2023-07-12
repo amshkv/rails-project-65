f1b40736386858a870a2fb38959727e6e0baa909
Andrey Moshkov <fakesupp@gmail.com>
2023-07-12 08:46:54 +0300

    :recycle: Move auth/policy uncludes for web controller

--- app/controllers/web/application_controller.rb
+++ app/controllers/web/application_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Web::ApplicationController < ApplicationController
+  include AuthConcern
+  include Pundit::Authorization
+
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   private
